8ba32f81056be25ee72b76749a9dcf66cdc425d5
Aaron Pham <contact@aarnphm.xyz>
2024-08-03 19:48:21 -0400

    perf(graph): initial canvas layout
    include nodes and links drawn
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -1,5 +1,6 @@
 import type { ContentDetails } from "../../plugins/emitters/contentIndex"
 import * as d3 from "d3"
+import * as PIXI from "pixi.js"
 import { registerEscapeHandler, removeAllChildren } from "./util"
 import { FullSlug, SimpleSlug, getFullSlug, resolveRelative, simplifySlug } from "../../util/path"
 
@@ -7,11 +8,22 @@ type NodeData = {
   id: SimpleSlug
   text: string
   tags: string[]
+
+  gfx?: {
+    container: PIXI.Text
+    alpha: number
+  }
 } & d3.SimulationNodeDatum
 
 type LinkData = {
   source: SimpleSlug
   target: SimpleSlug
+  gfx?: PIXI.Graphics
+}
+
+type LinkNodes = Omit<LinkData, "source" | "target"> & {
+  source: NodeData
+  target: NodeData
 }
 
 const localStorageKey = "graph-visited"
@@ -55,8 +67,10 @@ async function renderGraph(container: string, fullSlug: FullSlug) {
   )
   const links: LinkData[] = []
   const tags: SimpleSlug[] = []
-
   const validLinks = new Set(data.keys())
+  const height = Math.max(graph.offsetHeight, 250)
+  const width = graph.offsetWidth
+
   for (const [source, details] of data.entries()) {
     const outgoing = details.links ?? []
 
@@ -100,89 +114,46 @@ async function renderGraph(container: string, fullSlug: FullSlug) {
     if (showTags) tags.forEach((tag) => neighbourhood.add(tag))
   }
 
-  const graphData: { nodes: NodeData[]; links: LinkData[] } = {
+  // XXX: How does links got morphed into LinkNodes here?
+  // links => LinkData[], where as links.filter(l => neighbourhood.has(l.source) && neighbourhood.has(l.target)) => LinkNodes[]
+  const graphData: { nodes: NodeData[]; links: LinkNodes[] } = {
     nodes: [...neighbourhood].map((url) => {
-      const text = url.startsWith("tags/") ? "#" + url.substring(5) : data.get(url)?.title ?? url
+      const text = url.startsWith("tags/") ? "#" + url.substring(5) : (data.get(url)?.title ?? url)
       return {
         id: url,
         text: text,
         tags: data.get(url)?.tags ?? [],
       }
     }),
-    links: links.filter((l) => neighbourhood.has(l.source) && neighbourhood.has(l.target)),
+    links: links.filter(
+      (l) => neighbourhood.has(l.source) && neighbourhood.has(l.target),
+    ) as unknown as LinkNodes[],
   }
 
-  const simulation: d3.Simulation<NodeData, LinkData> = d3
-    .forceSimulation(graphData.nodes)
-    .force("charge", d3.forceManyBody().strength(-100 * repelForce))
-    .force(
-      "link",
-      d3
-        .forceLink(graphData.links)
-        .id((d: any) => d.id)
-        .distance(linkDistance),
-    )
-    .force("center", d3.forceCenter().strength(centerForce))
-
-  const height = Math.max(graph.offsetHeight, 250)
-  const width = graph.offsetWidth
-
-  const svg = d3
-    .select<HTMLElement, NodeData>("#" + container)
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [-width / 2 / scale, -height / 2 / scale, width / scale, height / scale])
-
-  // draw links between nodes
-  const link = svg
-    .append("g")
-    .selectAll("line")
-    .data(graphData.links)
-    .join("line")
-    .attr("class", "link")
-    .attr("stroke", "var(--lightgray)")
-    .attr("stroke-width", 1)
-
-  // svg groups
-  const graphNode = svg.append("g").selectAll("g").data(graphData.nodes).enter().append("g")
+  const computedStyleMap = new Map<string, string>()
+  for (let i of [
+    "--secondary",
+    "--tertiary",
+    "--gray",
+    "--light",
+    "--lightgray",
+    "--dark",
+    "--darkgray",
+    "--bodyFont",
+  ]) {
+    computedStyleMap.set(i, getComputedStyle(graph).getPropertyValue(i))
+  }
 
   // calculate color
   const color = (d: NodeData) => {
     const isCurrent = d.id === slug
     if (isCurrent) {
-      return "var(--secondary)"
+      return computedStyleMap.get("--secondary")
     } else if (visited.has(d.id) || d.id.startsWith("tags/")) {
-      return "var(--tertiary)"
+      return computedStyleMap.get("--tertiary")
     } else {
-      return "var(--gray)"
+      return computedStyleMap.get("--gray")
     }
-  }
-
-  const drag = (simulation: d3.Simulation<NodeData, LinkData>) => {
-    function dragstarted(event: any, d: NodeData) {
-      if (!event.active) simulation.alphaTarget(1).restart()
-      d.fx = d.x
-      d.fy = d.y
-    }
-
-    function dragged(event: any, d: NodeData) {
-      d.fx = event.x
-      d.fy = event.y
-    }
-
-    function dragended(event: any, d: NodeData) {
-      if (!event.active) simulation.alphaTarget(0)
-      d.fx = null
-      d.fy = null
-    }
-
-    const noop = () => {}
-    return d3
-      .drag<Element, NodeData>()
-      .on("start", enableDrag ? dragstarted : noop)
-      .on("drag", enableDrag ? dragged : noop)
-      .on("end", enableDrag ? dragended : noop)
   }
 
   function nodeRadius(d: NodeData) {
@@ -190,161 +161,239 @@ async function renderGraph(container: string, fullSlug: FullSlug) {
     return 2 + Math.sqrt(numLinks)
   }
 
-  let connectedNodes: SimpleSlug[] = []
+  let connectedNodes: NodeData[] = []
 
-  // draw individual nodes
-  const node = graphNode
-    .append("circle")
-    .attr("class", "node")
-    .attr("id", (d) => d.id)
-    .attr("r", nodeRadius)
-    .attr("fill", color)
-    .style("cursor", "pointer")
-    .on("click", (_, d) => {
-      const targ = resolveRelative(fullSlug, d.id)
-      window.spaNavigate(new URL(targ, window.location.toString()))
-    })
-    .on("mouseover", function (_, d) {
-      const currentId = d.id
-      const linkNodes = d3
-        .selectAll(".link")
-        .filter((d: any) => d.source.id === currentId || d.target.id === currentId)
+  const app = new PIXI.Application()
+  await app.init({
+    width,
+    height,
+    autoDensity: true,
+    backgroundAlpha: 0,
+    preference: "webgpu",
+    resolution: window.devicePixelRatio || 1,
+    eventMode: "dynamic",
+  })
+  graph.appendChild(app.canvas)
 
-      if (focusOnHover) {
-        // fade out non-neighbour nodes
-        connectedNodes = linkNodes.data().flatMap((d: any) => [d.source.id, d.target.id])
+  const stage = app.stage
+  stage.interactive = true
 
-        d3.selectAll<HTMLElement, NodeData>(".link")
-          .transition()
-          .duration(200)
-          .style("opacity", 0.2)
-        d3.selectAll<HTMLElement, NodeData>(".node")
-          .filter((d) => !connectedNodes.includes(d.id))
-          .transition()
-          .duration(200)
-          .style("opacity", 0.2)
+  // styling setup
+  const linkStyle: PIXI.StrokeStyle = {
+    width: 1,
+    color: computedStyleMap.get("--lightgray"),
+    alpha: 1,
+  }
 
-        d3.selectAll<HTMLElement, NodeData>(".node")
-          .filter((d) => !connectedNodes.includes(d.id))
-          .nodes()
-          .map((it) => d3.select(it.parentNode as HTMLElement).select("text"))
-          .forEach((it) => {
-            let opacity = parseFloat(it.style("opacity"))
-            it.transition()
-              .duration(200)
-              .attr("opacityOld", opacity)
-              .style("opacity", Math.min(opacity, 0.2))
-          })
-      }
+  const linksContainer = new PIXI.Container<PIXI.Graphics>()
+  const nodesContainer = new PIXI.Container<PIXI.Graphics>()
+  const labelsContainer = new PIXI.Container<PIXI.Text>()
 
-      // highlight links
-      linkNodes.transition().duration(200).attr("stroke", "var(--gray)").attr("stroke-width", 1)
+  stage.addChild(linksContainer, nodesContainer, labelsContainer)
 
-      const bigFont = fontSize * 1.5
-
-      // show text for self
-      const parent = this.parentNode as HTMLElement
-      d3.select<HTMLElement, NodeData>(parent)
-        .raise()
-        .select("text")
-        .transition()
-        .duration(200)
-        .attr("opacityOld", d3.select(parent).select("text").style("opacity"))
-        .style("opacity", 1)
-        .style("font-size", bigFont + "em")
-    })
-    .on("mouseleave", function (_, d) {
-      if (focusOnHover) {
-        d3.selectAll<HTMLElement, NodeData>(".link").transition().duration(200).style("opacity", 1)
-        d3.selectAll<HTMLElement, NodeData>(".node").transition().duration(200).style("opacity", 1)
-
-        d3.selectAll<HTMLElement, NodeData>(".node")
-          .filter((d) => !connectedNodes.includes(d.id))
-          .nodes()
-          .map((it) => d3.select(it.parentNode as HTMLElement).select("text"))
-          .forEach((it) => it.transition().duration(200).style("opacity", it.attr("opacityOld")))
-      }
-      const currentId = d.id
-      const linkNodes = d3
-        .selectAll(".link")
-        .filter((d: any) => d.source.id === currentId || d.target.id === currentId)
-
-      linkNodes.transition().duration(200).attr("stroke", "var(--lightgray)")
-
-      const parent = this.parentNode as HTMLElement
-      d3.select<HTMLElement, NodeData>(parent)
-        .select("text")
-        .transition()
-        .duration(200)
-        .style("opacity", d3.select(parent).select("text").attr("opacityOld"))
-        .style("font-size", fontSize + "em")
-    })
-    // @ts-ignore
-    .call(drag(simulation))
-
-  // make tags hollow circles
-  node
-    .filter((d) => d.id.startsWith("tags/"))
-    .attr("stroke", color)
-    .attr("stroke-width", 2)
-    .attr("fill", "var(--light)")
-
-  // draw labels
-  const labels = graphNode
-    .append("text")
-    .attr("dx", 0)
-    .attr("dy", (d) => -nodeRadius(d) + "px")
-    .attr("text-anchor", "middle")
-    .text((d) => d.text)
-    .style("opacity", (opacityScale - 1) / 3.75)
-    .style("pointer-events", "none")
-    .style("font-size", fontSize + "em")
-    .raise()
-    // @ts-ignore
-    .call(drag(simulation))
-
-  // set panning
-  if (enableZoom) {
-    svg.call(
+  const simulation: d3.Simulation<NodeData, LinkNodes> = d3
+    .forceSimulation(graphData.nodes)
+    .force("charge", d3.forceManyBody().strength(-100 * repelForce))
+    .force("center", d3.forceCenter(width / 2, height / 2).strength(centerForce / 2))
+    .force(
+      "link",
       d3
-        .zoom<SVGSVGElement, NodeData>()
+        .forceLink(graphData.links)
+        .id((d: any) => d.id)
+        .distance(linkDistance),
+    )
+    .force(
+      "collide",
+      d3.forceCollide((n) => (nodeRadius(n) + 20) / 3.75),
+    )
+    .on("tick", () => {
+      //progress the simulation
+      nodesContainer.children.forEach((node) => {
+        const d = graphData.nodes.find((n) => n.id === node.label)
+        if (d) {
+          node.updateTransform({ x: d.x!, y: d.y! })
+          if (d.gfx === undefined) {
+            const alpha = (opacityScale - 1) / 3.75
+            const container = new PIXI.Text({
+              text: d.text,
+              alpha,
+              zIndex: 100,
+              anchor: { x: 0.5, y: -0.5 },
+              style: {
+                fontSize: fontSize,
+                fill: computedStyleMap.get("--dark"),
+                fontFamily: computedStyleMap.get("--bodyFont"),
+              },
+              resolution: window.devicePixelRatio || 1,
+              interactive: false,
+            })
+            d.gfx = { alpha, container }
+            labelsContainer.addChild(container)
+          }
+          d.gfx.container.updateTransform({ x: d.x!, y: d.y! })
+        }
+      })
+
+      graphData.links.forEach((link) => {
+        if (link.gfx === undefined) {
+          link.gfx = new PIXI.Graphics({ zIndex: 1 }).setStrokeStyle(linkStyle)
+          linksContainer.addChild(link.gfx)
+        }
+        link.gfx
+          ?.clear()
+          .moveTo(link!.source.x!, link!.source.y!)
+          .lineTo(link!.target.x!, link!.target.y!)
+          .stroke()
+      })
+    })
+
+  graphData.nodes.forEach((n) => {
+    const nodeId = n.id
+    const bigFont = fontSize * 2
+
+    // TODO: transition, fade
+    const node = new PIXI.Graphics({
+      interactive: true,
+      label: nodeId,
+      x: n.x,
+      y: n.y,
+      eventMode: "dynamic",
+      zIndex: 50,
+    })
+      .circle(0, 0, nodeRadius(n))
+      .on("click", () => {
+        const targ = resolveRelative(fullSlug, n.id)
+        window.spaNavigate(new URL(targ, window.location.toString()))
+      })
+      .on("mouseover", () => {
+        const linksFromNode = graphData.links.filter(
+          (d) => d.source.id === nodeId || d.target.id === nodeId,
+        )
+        linksFromNode.forEach((link) => {
+          link.gfx
+            ?.clear()
+            .moveTo(link.source.x!, link.source.y!)
+            .lineTo(link.target.x!, link.target.y!)
+            .stroke({ color: computedStyleMap.get("--darkgray") })
+        })
+
+        if (n.gfx) {
+          n.gfx.container.style.fontSize = bigFont
+          n.gfx.container.alpha = 1
+        }
+
+        if (focusOnHover) {
+          // Fade out non-neighbour nodes
+          connectedNodes = linksFromNode.flatMap((d) => [d.source, d.target])
+
+          nodesContainer.children.forEach((node) => {
+            if (![...connectedNodes].map((i) => i.id).includes(node.label as SimpleSlug)) {
+              node.alpha = 0.2
+            }
+          })
+          connectedNodes.forEach((nd) => {
+            nd.gfx!.container.alpha = 1
+          })
+          graphData.links.forEach((link) => {
+            if (!linksFromNode.includes(link)) {
+              link.gfx!.alpha = 0.2
+            }
+          })
+        }
+      })
+      .on("mouseleave", () => {
+        const linksFromNode = graphData.links.filter(
+          (d) => d.source.id === nodeId || d.target.id === nodeId,
+        )
+        linksFromNode.forEach((link) => {
+          link.gfx
+            ?.clear()
+            .moveTo(link.source.x!, link.source.y!)
+            .lineTo(link.target.x!, link.target.y!)
+            .stroke({ color: computedStyleMap.get("--lightgray") })
+        })
+
+        if (n.gfx) {
+          n.gfx.container.style.fontSize = fontSize
+          n.gfx.container.alpha = n.gfx.alpha
+        }
+
+        if (focusOnHover) {
+          nodesContainer.children.forEach((node) => {
+            if (![...connectedNodes].map((i) => i.id).includes(node.label as SimpleSlug)) {
+              node.alpha = 1
+            }
+          })
+          connectedNodes.forEach((nd) => {
+            nd.gfx!.container.alpha = nd.gfx!.alpha
+          })
+          graphData.links.forEach((link) => {
+            if (!linksFromNode.includes(link)) {
+              link.gfx!.alpha = 1
+            }
+          })
+        }
+      })
+
+    if (n.id.startsWith("tags/")) {
+      node.fill({ color: computedStyleMap.get("--light") }).stroke({ width: 0.5, color: color(n) })
+    } else {
+      node.fill(color(n)).stroke({ color: color(n) })
+    }
+
+    nodesContainer.addChild(node)
+  })
+
+  const d3Canvas = d3.select<HTMLCanvasElement, NodeData>(app.canvas)
+
+  // Set up zoom and pan
+  if (enableZoom) {
+    d3Canvas.call(
+      d3
+        .zoom<HTMLCanvasElement, NodeData>()
         .extent([
           [0, 0],
           [width, height],
         ])
         .scaleExtent([0.25, 4])
         .on("zoom", ({ transform }) => {
-          link.attr("transform", transform)
-          node.attr("transform", transform)
+          stage.scale.set(transform.k)
+          stage.position.set(transform.x, transform.y)
+
           const scale = transform.k * opacityScale
           const scaledOpacity = Math.max((scale - 1) / 3.75, 0)
-          labels.attr("transform", transform).style("opacity", scaledOpacity)
+          graphData.nodes.forEach((el) => {
+            el.gfx!.container.alpha = scaledOpacity
+            el.gfx!.alpha = scaledOpacity
+          })
         }),
     )
   }
 
-  // progress the simulation
-  simulation.on("tick", () => {
-    link
-      .attr("x1", (d: any) => d.source.x)
-      .attr("y1", (d: any) => d.source.y)
-      .attr("x2", (d: any) => d.target.x)
-      .attr("y2", (d: any) => d.target.y)
-    node.attr("cx", (d: any) => d.x).attr("cy", (d: any) => d.y)
-    labels.attr("x", (d: any) => d.x).attr("y", (d: any) => d.y)
-  })
+  // FIXME: implement scaling for HiDPI screens, and weird font scaling
+
+  // FIXME: implement dragging logics per node to and simulate with links and labels
 }
 
-function renderGlobalGraph() {
-  const slug = getFullSlug(window)
+document.addEventListener("nav", async (e: CustomEventMap["nav"]) => {
+  const slug = e.detail.url
+  addToVisited(simplifySlug(slug))
+  await renderGraph("graph-container", slug)
+
   const container = document.getElementById("global-graph-outer")
   const sidebar = container?.closest(".sidebar") as HTMLElement
-  container?.classList.add("active")
-  if (sidebar) {
-    sidebar.style.zIndex = "1"
-  }
 
-  renderGraph("global-graph-container", slug)
+  function renderGlobalGraph() {
+    const slug = getFullSlug(window)
+    container?.classList.add("active")
+    if (sidebar) {
+      sidebar.style.zIndex = "1"
+    }
+
+    renderGraph("global-graph-container", slug)
+
+    registerEscapeHandler(container, hideGlobalGraph)
+  }
 
   function hideGlobalGraph() {
     container?.classList.remove("active")
@@ -356,15 +405,18 @@ function renderGlobalGraph() {
     removeAllChildren(graph)
   }
 
-  registerEscapeHandler(container, hideGlobalGraph)
-}
-
-document.addEventListener("nav", async (e: CustomEventMap["nav"]) => {
-  const slug = e.detail.url
-  addToVisited(simplifySlug(slug))
-  await renderGraph("graph-container", slug)
+  async function shortcutHandler(e: HTMLElementEventMap["keydown"]) {
+    if (e.key === "g" && (e.ctrlKey || e.metaKey) && !e.shiftKey) {
+      e.preventDefault()
+      const globalGraphOpen = container?.classList.contains("active")
+      globalGraphOpen ? hideGlobalGraph() : renderGlobalGraph()
+    }
+  }
 
   const containerIcon = document.getElementById("global-graph-icon")
   containerIcon?.addEventListener("click", renderGlobalGraph)
   window.addCleanup(() => containerIcon?.removeEventListener("click", renderGlobalGraph))
+
+  document.addEventListener("keydown", shortcutHandler)
+  window.addCleanup(() => document.removeEventListener("keydown", shortcutHandler))
 })
