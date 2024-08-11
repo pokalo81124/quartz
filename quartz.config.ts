05398e40c3e3efde3ee9335d7acca98ea0d5a29f
pokalo81124 <77443113+pokalo81124@users.noreply.github.com>
2024-08-11 21:20:11 +0800

    1

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
     baseUrl: "eki.naturaleki.one",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
