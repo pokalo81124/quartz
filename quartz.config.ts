cc2b87a7543644a3e08cda6b4a075535e7ed7037
pokalo81124 <77443113+pokalo81124@users.noreply.github.com>
2024-08-11 22:33:58 +0800

    1

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Noto Serif Simplified Chinese",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
