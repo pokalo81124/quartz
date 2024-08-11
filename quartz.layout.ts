e98b93ddd8aef87cd3032a8c321a69199ae2f18f
pokalo81124 <77443113+pokalo81124@users.noreply.github.com>
2024-08-11 22:56:27 +0800

    1

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,7 +26,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.RecentNotes()),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
